9b59dfcf12fb2938c7b29a3264c3db7dba605bd0
ghkdtjsdud0312 <ghkdtjsdud0312@gmail.com>
2023-08-17 12:40:38 +0900

    인테리제에서 컴밋

--- java_k_digital_0814/src/형변환/TypeCasting.java
+++ java_k_digital_0814/src/형변환/TypeCasting.java
@@ -18,6 +18,9 @@ public class TypeCasting {
             double avg = (double) total / 3; //3도 실수형으로 바뀜
 
             System.out.println(avg);
+            System.out.println(avg);
+            System.out.println(avg);
+
         }
 }
 
