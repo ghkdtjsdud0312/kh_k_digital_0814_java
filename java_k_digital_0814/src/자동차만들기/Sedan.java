12b69c7d207641c07f7d75ed469abd126d2ff1a8
ghkdtjsdud0312 <ghkdtjsdud0312@gmail.com>
2023-08-28 15:58:12 +0900

    자동차만들기

---
+++ java_k_digital_0814/src/자동차만들기/Sedan.java
@@ -0,0 +1,15 @@
+package 자동차만들기;
+
+public class Sedan extends Car{
+    public Sedan(String name) {
+        this.name = name;
+        maxSpeed = 200;
+        fuelEff = 12;
+        fuelTank = 45;
+        seatCnt = 4;
+    }
+    @Override
+    void setMode(boolean isMode) {
+        if(isMode) seatCnt += 1;
+    }
+}
