f39fd8955e0b974c1e745a40f7baa88e2bd77dfc
ghkdtjsdud0312 <ghkdtjsdud0312@gmail.com>
2023-08-30 16:55:04 +0900

    x보다 작은 수

--- java_k_digital_0814/src/X보다작은수/Ex.java
+++ java_k_digital_0814/src/X보다작은수/Ex.java
@@ -8,16 +8,13 @@ public class Ex {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         List<Integer> list = new ArrayList<>();
-        int n = sc.nextInt();
-        int x = sc.nextInt();
+        int n = sc.nextInt(); // 배열의 개수
+        int x = sc.nextInt(); // 크기를 비교하는 기준 값(미만 값을 찾음)
         for (int i = 0; i < n; i++) {
-            list.add(sc.nextInt());
-
-            if (list.get(i) < x) {
-                System.out.print(list.get(i) + " ");
-            }
-            System.out.println();
+            list.add(sc.nextInt()); // 리스트에 값을 추가 함
+            if (list.get(i) < x) System.out.print(list.get(i) + " ");
         }
+        System.out.println();
     }
 
 }
