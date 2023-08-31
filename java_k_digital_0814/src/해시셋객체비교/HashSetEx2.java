bf9619fb5ebc24c04a9fcf1b8515fbb559735456
ghkdtjsdud0312 <ghkdtjsdud0312@gmail.com>
2023-08-31 15:19:41 +0900

    해시셋객체비교

--- java_k_digital_0814/src/해시셋객체비교/HashSetEx2.java
+++ java_k_digital_0814/src/해시셋객체비교/HashSetEx2.java
@@ -15,9 +15,16 @@ public class HashSetEx2 {
         hashSet.add(member2);
         hashSet.add(member3);
         hashSet.add(member4); // 같은 객체 인지 equal() 사용해 확인 한다.
-        for(Member e : hashSet) e.showMember();
+        //  for(Member e : hashSet) e.showMember();
         // 객체 주소가 다르면 같은 값이여도 중복 제거 안되고 그대로 나옴
         // 중복 제거를 하고 싶으면 오버라이딩을 해야 한다.
+
+//        HashSet<Integer> s1 = new HashSet<>(Arrays.asList(1,2,3,4,5,6));
+//        HashSet<Integer> s2 = new HashSet<>(Arrays.asList(4,5,6,7,8,9));
+//        s1.addAll(s2); // 합집합 : 양쪽 모두에 있는 것, 중복 제거
+//        s1.retainAll(s2); // 교집합 : 양쪽 모두 존재하는 항목
+//        s1.removeAll(s2) // 차집합 : 앞에서 뒤를 뺴는 것
+//        System.out.println(s1);
 
     }
 }
