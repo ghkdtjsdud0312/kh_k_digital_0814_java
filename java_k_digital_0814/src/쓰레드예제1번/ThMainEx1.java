8d76485eb7205f8a0cbaa806c5d8cbb39b5632a1
ghkdtjsdud0312 <ghkdtjsdud0312@gmail.com>
2023-09-01 12:50:12 +0900

    쓰레드

---
+++ java_k_digital_0814/src/쓰레드예제1번/ThMainEx1.java
@@ -0,0 +1,39 @@
+package 쓰레드예제1번;
+    // 하나의 프로그램 내에서 여러 가지 일을 동시에 수행 하는 것
+public class ThMainEx1 {
+    public static void main(String[] args) {
+        Thread testTh = new CustomThread();
+        testTh.start(); // 운영체제한테 시작해달라고 요청함->운영체제가 run불러줌
+
+
+        Runnable runTask = new InterThread(); // 실제 작업 내용을 가지고 있지만 실제 쓰레드는 아님
+        Thread runTh1 = new Thread(runTask);
+        runTh1.start();
+        System.out.println("Main에서 찍어 봅니다.");
+    }
+}
+// 상속 받아서 쓰레드를 만드는 방법(좋은 방법은 아님)
+class CustomThread extends Thread {
+    @Override
+    public void run() { // run
+        int sum = 0;
+        for(int i = 0; i <= 10; i++) {
+            sum += i;
+            System.out.println("상속 받은 쓰레드 : " + sum);
+        }
+        System.out.println(Thread.currentThread() + "합" + sum);
+    }
+}
+// Runnable를 interThread로 만드는 방법
+class InterThread implements Runnable {
+
+    @Override
+    public void run() {
+        int sum = 0;
+        for(int i = 0; i <= 10; i++) {
+            sum += i;
+            System.out.println("인터페이스 : " + sum);
+        }
+        System.out.println(Thread.currentThread() + "합" + sum);
+    }
+}
