8948fc0d751406ad488ee0653ef1888ea5254112
ghkdtjsdud0312 <ghkdtjsdud0312@gmail.com>
2023-09-04 17:26:42 +0900

    9/4

--- java_k_digital_0814/src/스트링예제2번/StreamEx2.java
+++ java_k_digital_0814/src/스트링예제2번/StreamEx2.java
@@ -1,14 +1,11 @@
 package 스트링예제2번;
 
-import 클래스기본.Student;
-
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.IntStream;
 
 // 반복자 스트링 forEach() : 내부 반복자, 스트림 요소에 대한 순차 접근을 제공하며, 최종 연산에 해당 합니다.
-public class StringEx2 {
+public class StreamEx2 {
     public static void main(String[] args) {
 //        ArrayList<String> list = new ArrayList<>(Arrays.asList("고유림", "나희도", "백이진"));
 //        for(String e : list) System.out.print(e + " ");
